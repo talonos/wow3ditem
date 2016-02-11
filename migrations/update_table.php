fa9dad918f13ef604b02ba25def83da813d11504
talonos <targetdvds@hotmail.com>
2016-02-11 17:25:35 +0000

    travis fixes

--- migrations/update_table.php
+++ migrations/update_table.php
@@ -12,7 +12,7 @@ class update_table extends \phpbb\db\migration\migration
 	public function update_data()
 	{
 		return array(
-		 array('custom', array(array($this, 'install_bbcode_for_3ditem'))),
+		array('custom', array(array($this, 'install_bbcode_for_3ditem'))),
 		);
 	}
 	public function revert_data()
@@ -27,57 +27,56 @@ class update_table extends \phpbb\db\migration\migration
 
 		if (!class_exists('acp_bbcodes'))
 		{
-		 include($this->phpbb_root_path . 'includes/acp/acp_bbcodes.' . $this->php_ext);
+		include($this->phpbb_root_path . 'includes/acp/acp_bbcodes.' . $this->php_ext);
 		}
 		$bbcode_tool = new \acp_bbcodes();
 
 		$bbcode_name = '3ditem';
 		$bbcode_array = array(
-		 'bbcode_match'		=> '[3ditem]{NUMBER}[/3ditem]',
-		 'bbcode_tpl'		=> '
-		 <script type="text/javascript">
-r(function(){
+		'bbcode_match'		=> '[3ditem]{NUMBER}[/3ditem]',
+		'bbcode_tpl'		=> '
+		<script type="text/javascript">
+		r(function(){
 		    $(function(){
-      $("#model-{NUMBER}").spritespin({
-        width     : 280,
-        height    : 280,
-        frames    : 24,
-        image     : "http://media.blizzard.com/wow/renders/items/item{NUMBER}.jpg",
-      animate   : false,
-        loop      : true,
-      });
+			$("#model-{NUMBER}").spritespin({
+			width     : 280,
+			height		: 280,
+			frames		: 24,
+			image		: "http://media.blizzard.com/wow/renders/items/item{NUMBER}.jpg",
+			animate		: false,
+			loop		: true,
+				});
+			});
+		});
 
-    });
-
-    });
-    function r(f){/in/.test(document.readyState)?setTimeout("r("+f+")",9):f()}
-    </script>
-  <div>
-  <div align="center" width="280px" height="280px" ><div id="item-{NUMBER}"><a href="http://www.wowhead.com/item={NUMBER}"></a></div>
-    <div class="model-viewer" id="model-{NUMBER}"></div>
-   
-</div>
-</div>
-
-',
-		 'bbcode_helpline'	=> '[3ditem]ITEM ID NUMBER[/3ditem]',
-		 'display_on_posting'	=> 1
+		function r(f){/in/.test(document.readyState)?setTimeout("r("+f+")",9):f()}
+		</script>
+		<div>
+			<div align="center" width="280px" height="280px" >
+					<div id="item-{NUMBER}"><a href="http://www.wowhead.com/item={NUMBER}"></a></div>
+				<div class="model-viewer" id="model-{NUMBER}">
+				</div>
+			</div>
+		</div>
+		',
+		'bbcode_helpline'	=> '[3ditem]ITEM ID NUMBER[/3ditem]',
+		'display_on_posting'	=> 1
 		);
 
 		$data = $bbcode_tool->build_regexp($bbcode_array['bbcode_match'], $bbcode_array['bbcode_tpl'], $bbcode_array['bbcode_helpline']);
 
 		$bbcode_array += array(
-		 'bbcode_tag'			=> $data['bbcode_tag'],
-		 'first_pass_match'	=> $data['first_pass_match'],
-		 'first_pass_replace'	=> $data['first_pass_replace'],
-		 'second_pass_match'	=> $data['second_pass_match'],
-		 'second_pass_replace' => $data['second_pass_replace']
+			'bbcode_tag'			=> $data['bbcode_tag'],
+			'first_pass_match'		=> $data['first_pass_match'],
+			'first_pass_replace'	=> $data['first_pass_replace'],
+			'second_pass_match'		=> $data['second_pass_match'],
+			'second_pass_replace'	=> $data['second_pass_replace']
 		);
 
 		$sql = 'SELECT bbcode_id
-		 FROM ' . $this->table_prefix . "bbcodes
-		 WHERE LOWER(bbcode_tag) = '" . strtolower($bbcode_name) . "'
-		 OR LOWER(bbcode_tag) = '" . strtolower($bbcode_array['bbcode_tag']) . "'";
+			FROM ' . $this->table_prefix . "bbcodes
+			WHERE LOWER(bbcode_tag) = '" . strtolower($bbcode_name) . "'
+			OR LOWER(bbcode_tag) = '" . strtolower($bbcode_array['bbcode_tag']) . "'";
 		$result = $this->db->sql_query($sql);
 		$row_exists = $this->db->sql_fetchrow($result);
 		$this->db->sql_freeresult($result);
@@ -85,44 +84,42 @@ r(function(){
 		if ($row_exists)
 		{
 
-		 $bbcode_id = $row_exists['bbcode_id'];
+		$bbcode_id = $row_exists['bbcode_id'];
 
-		 $sql = 'UPDATE ' . $this->table_prefix . 'bbcodes
+		$sql = 'UPDATE ' . $this->table_prefix . 'bbcodes
 			SET ' . $this->db->sql_build_array('UPDATE', $bbcode_array) . '
 			WHERE bbcode_id = ' . $bbcode_id;
-		 $this->db->sql_query($sql);
+		$this->db->sql_query($sql);
 		}
 		else
 		{
-
-		 $sql = 'SELECT MAX(bbcode_id) AS max_bbcode_id
+		$sql = 'SELECT MAX(bbcode_id) AS max_bbcode_id
 			FROM ' . $this->table_prefix . 'bbcodes';
-		 $result = $this->db->sql_query($sql);
-		 $row = $this->db->sql_fetchrow($result);
-		 $this->db->sql_freeresult($result);
+		$result = $this->db->sql_query($sql);
+		$row = $this->db->sql_fetchrow($result);
+		$this->db->sql_freeresult($result);
 
-		 if ($row)
-		 {
+		if ($row)
+		{
 			$bbcode_id = $row['max_bbcode_id'] + 1;
 
 			if ($bbcode_id <= NUM_CORE_BBCODES)
 			{
 				$bbcode_id = NUM_CORE_BBCODES + 1;
 			}
-		 }
-		 else
-		 {
+		}
+		else
+		{
 			$bbcode_id = NUM_CORE_BBCODES + 1;
-		 }
+		}
 
-		 if ($bbcode_id <= BBCODE_LIMIT)
-		 {
+		if ($bbcode_id <= BBCODE_LIMIT)
+		{
 			$bbcode_array['bbcode_id'] = (int) $bbcode_id;
-
 			$this->db->sql_query('INSERT INTO ' . $this->table_prefix . 'bbcodes ' . $this->db->sql_build_array('INSERT', $bbcode_array));
-		 }
 		}
 	}
+}
 
 	public function remove_bbcode()
 	{
